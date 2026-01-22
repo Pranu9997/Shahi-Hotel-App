7b739f68e57ae2f8c05a14827e49a909aa7c1771
Pranu9997 <Pranavyadav5594@gmail.com>
2026-01-22 19:22:31 +0530

    fix port for Render

--- app.py
+++ app.py
@@ -262,6 +262,9 @@ def health():
 # -------------------------
 # RUN SERVER
 # -------------------------
+import os
+
+port = int(os.environ.get("PORT", 5000))
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
 
